d5b765572f76830a3167641502223c3853f3874a
nominb <nominboldbaatar4@gmail.com>
2018-09-28 09:56:59 -0400

    images working

--- Friend-finder/app/data/friends.js
+++ Friend-finder/app/data/friends.js
@@ -2,15 +2,15 @@ var friends = [
 
 {
     "name":"Alec",
-    "photo":"https://www.gettyimages.com/detail/news-photo/alec-baldwin-episode-1718-pictured-melissa-mccarthy-as-news-photo/635127138",
+    "photo":"https://www.jetss.com/wp-content/uploads/2018/07/184.jpg",
     "scores":[ 5,1,4,4,5,1,2,5,4,1]
 },
 {   "name":"Ralph",
-    "photo":"https://www.gettyimages.com/detail/news-photo/kristen-stewart-episode-1717-pictured-melissa-mccarthy-as-news-photo/633937832",
+    "photo":"https://media.npr.org/assets/img/2017/02/12/melissamccarthy_wide-da866c3b03608852d883a38fb4e715836dac0d63.png?s=1400",
     "scores":[ 3,4,2,3,5,1,2,2,3,1]
 },
 {   "name":"Tara",
-    "photo":"/images/pic.jpg",
+    "photo":"https://media.merryjane.com/eVkCZIdjqQ2ME4J8GGKNF_VOoo8=/0x500/files.merryjane.com/uploads/article/hero_image/6554/heady_entertainment_may_11th__WIDE.jpg",
     "scores":[ 3,2,5,3,3,2,1,3,4,2]
 },
 {   "name":"Mellisa",
@@ -18,11 +18,11 @@ var friends = [
     "scores":[2,5,2,1,5,4,3,2,4,2]
 },
 {   "name":"Rose",
-    "photo":"https://www.gettyimages.com/detail/news-photo/actress-melissa-mccarthy-is-photographed-for-the-guardian-news-photo/541962616",
+    "photo":"https://filmschoolrejects.com/wp-content/uploads/2018/04/melissa-mccarthy-spy.jpg",
     "scores":[3,1,5,4,2,3,3,3,3,3]
 },
 {   "name":"Loretta",
-    "photo":"https://www.gettyimages.com/detail/news-photo/melissa-mccarthy-episode-1724-pictured-melissa-mccarthy-news-photo/682062798",
+    "photo":"https://imgix.bustle.com/uploads/image/2017/8/22/4dbabff7-c8bd-4817-abff-a413fd946e49-joey-tribbiani-pineapple.jpg?w=970&h=582&fit=crop&crop=faces&auto=format&q=70",
     "scores":[1,1,4,4,5,1,3,2,4,5]
 }
 
